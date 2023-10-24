4f7663525dd4f7fc709f617781a9af0bcc6ca821
Seth Phat <phattranminh96@gmail.com>
2023-10-24 13:45:25 +0700

    ShipSaaS Logger

--- src/ShipSaasUniqueRequestLoggerServiceProvider.php
+++ src/ShipSaasUniqueRequestLoggerServiceProvider.php
@@ -3,7 +3,10 @@
 namespace ShipSaasUniqueRequestLogger;
 
 use Illuminate\Foundation\Console\AboutCommand;
+use Illuminate\Log\LogManager;
 use Illuminate\Support\ServiceProvider;
+use Monolog\Logger as Monolog;
+use ShipSaasUniqueRequestLogger\Processor\UniqueRequestIdProcessor;
 
 class ShipSaasUniqueRequestLoggerServiceProvider extends ServiceProvider
 {
@@ -13,9 +16,28 @@ class ShipSaasUniqueRequestLoggerServiceProvider extends ServiceProvider
     {
         if ($this->app->runningInConsole()) {
             AboutCommand::add(
-                'ShipSaaS: Laravel Unique Request Id Logger',
+                'ShipSaaS: Logger',
                 fn () => ['Version' => static::VERSION]
             );
         }
+
+        $this->app->afterResolving(LogManager::class, function (LogManager $manager) {
+            $manager->extend('shipsaas-logger', function (array $config) {
+                /** @var LogManager $this */
+
+                $handler = $this->channel('stack');
+
+                return new Monolog(
+                    'shipsaas-logger',
+                    [
+                        ...$handler->getHandlers(),
+                    ],
+                    [
+                        ...$handler->getProcessors(),
+                        $this->app->make(UniqueRequestIdProcessor::class)
+                    ]
+                );
+            });
+        });
     }
 }
